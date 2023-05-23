b8273227bac63fb09511d91bc3b99992c8ffd491
brunollino <brunolino775@gamil.com>
2023-05-23 17:35:53 -0300

    Exercicio 63

--- frontend/src/addons/intranet_interagi/src/customizations/volto/components/theme/Header/Header.jsx
+++ frontend/src/addons/intranet_interagi/src/customizations/volto/components/theme/Header/Header.jsx
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import Container from '@kitconcept/volto-light-theme/components/Atoms/Container/Container';
-
-
+import { ShowBookmarksContentButton } from '@plone-collective/volto-bookmarks/components';
 import {
   Anontools,
   LanguageSelector,
@@ -32,10 +31,9 @@ const Header = (props) => {
           </div>
           <div className="tools-wrapper">
             <LanguageSelector />
-
             <div className="tools">
               {!token && <Anontools />}
-
+              {token && <ShowBookmarksContentButton />}
             </div>
           </div>
         </div>
