987258c7f675f21212950d40b389b3e68ea7ae08
brunollino <brunolino775@gamil.com>
2023-05-24 14:43:56 -0300

    Add packages

--- backend/src/intranet_interagi/src/intranet_interagi/subscribers/__init__.py
+++ backend/src/intranet_interagi/src/intranet_interagi/subscribers/__init__.py
@@ -5,6 +5,3 @@ def catch_all(event):
     klass_name = event.__class__
     atributos = ", ".join([attr for attr in dir(event) if not attr.startswith("_")])
     logger.info(f"EVENTO: {klass_name} - {atributos}")
-
-
-## teste commit
